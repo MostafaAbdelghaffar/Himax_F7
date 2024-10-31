012ff5e2a57bd8c7f542f0c52d800a623615327e
Abdelghaffar, Mostafa I <mostafa.abdelghaffar@te.com>
2024-10-31 11:39:44 +0000

    Anas's commit

--- Core/Inc/hm01b0.h
+++ Core/Inc/hm01b0.h
@@ -8,6 +8,9 @@
 #ifndef INC_HM01B0_H_
 #define INC_HM01B0_H_
 
+void HM01B0_WriteRegister(I2C_HandleTypeDef *hi2c, uint16_t reg_addr, uint8_t value);
+uint8_t HM01B0_ReadRegister(I2C_HandleTypeDef *hi2c, uint16_t reg_addr);
+void HM01B0_Init(void);
 
 
 #endif /* INC_HM01B0_H_ */
